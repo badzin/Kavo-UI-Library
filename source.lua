677b5a02b7803d46d2e1085d48cbaec3d26fdf5f
IlI <91797231+badzin@users.noreply.github.com>
2022-04-11 18:40:40 -0300

    Update source.lua

--- source.lua
+++ source.lua
@@ -113,10 +113,10 @@ local themeStyles = {
         ElementColor = Color3.fromRGB(24, 24, 24)
     },
     Serpent = {
-        SchemeColor = Color3.fromRGB(0, 166, 58),
+        SchemeColor = Color3.fromRGB(31, 41, 43),
         Background = Color3.fromRGB(31, 41, 43),
         Header = Color3.fromRGB(22, 29, 31),
-        TextColor = Color3.fromRGB(255,255,255),
+        TextColor = Color3.fromRGB(0, 166, 58),
         ElementColor = Color3.fromRGB(22, 29, 31)
     }
 }
